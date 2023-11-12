3783786b29e7d5564214533de76be02e0b68cbf2
Sylvain Vallot <29737056+sylvainvallot@users.noreply.github.com>
2023-11-12 20:01:09 +0100

    update logs fetching

--- tools/taskCronCleanLogs.js
+++ tools/taskCronCleanLogs.js
@@ -19,23 +19,34 @@ client.on('ready', async () => {
 
     const logs = await client.channels.cache.get(process.env.CHAN_ID_LOG_MSG)
     console.log('Logs channel found, fetching messages...');
-	const messages = await logs.messages.fetch({limit: 100});
 
 	const oneWeekAgo = new Date();
-	oneWeekAgo.setDate(oneWeekAgo.getDate() - 14);
+	oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
 
-	const toClean = await messages.filter(m => m.createdTimestamp < oneWeekAgo.getTime());
+	let messages;
+	let lastMessageId;
+	let i = 0;
 
-	if(toClean.size > 0){
-		console.log(`Found ${toClean.size} messages to clean, deleting...`);
-		for (const m of toClean.values()) {
-			
-			await m.delete();
-			
-			// wait 1 second to avoid rate limit
-			await new Promise(resolve => setTimeout(resolve, 1000));
+	do {
+		messages = await logs.messages.fetch({ limit: 100, before: lastMessageId });
+		const toClean = messages.filter(m => m.createdTimestamp < oneWeekAgo.getTime());		
+		
+		if (toClean.size > 0) {
+			console.log(`Found ${toClean.size} logs to clean, deleting...`);
+			for (const m of toClean.values()) {
+				await m.delete();
+				// wait 1 second to avoid rate limit
+				await new Promise(resolve => setTimeout(resolve, 1000));
+			}
+		} 
+		if (messages.size > 0) {
+			lastMessageId = messages.last().id;
+		} else {
+			lastMessageId = null;
 		}
-	}
+		i++;
+	} while (messages.size > 0 && i < 10);
+
 	console.log('Cleanings logs done, exiting');
 	process.exit();
 });
